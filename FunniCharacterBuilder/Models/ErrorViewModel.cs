1d57f975a5b47ea1944ee5a93c606fa63d3b3f0a
LysBlissful <Lys.cook369@gmail.com>
2025-10-13 17:09:27 -0400

    added example save file

---
+++ FunniCharacterBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FunniCharacterBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
