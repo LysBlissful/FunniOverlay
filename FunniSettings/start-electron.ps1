2ee97662acaaf42044b96129da50e76e3fb52a82
Joao-GitH <joao.prrvz@eduge.ch>
2026-01-08 21:36:57 +0100

    Add Powershell scripts to run the app and to build it into an executable

---
+++ FunniSettings/start-electron.ps1
@@ -0,0 +1,14 @@
+try {
+    Write-Host "Checking dependencies..."
+    if (-not (Test-Path "node_modules")) {
+        Write-Host "Installing dependencies..."
+        npm install
+    }
+
+    Write-Host "Launching Electron..."
+    npm run electron
+}
+catch {
+    Write-Host "An error occurred:"
+    Write-Host $_
+}
